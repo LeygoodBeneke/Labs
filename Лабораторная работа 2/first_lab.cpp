4ddc88682d9e71f1ea943e85428fe028d13fa53b
VasiliyDenisov <mr.v.m.denisov@mail.ru>
2022-11-04 15:28:45 +0300

    Add solution 1 task 2 lab

---
+++ Лабораторная работа 2/first_lab.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<iomanip>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    double first, second;
+    std:: cin >> first >> second;
+    std:: cout << "Среднее арифметическое двух введённых чисел: " << (first + second) / 2;
+
+    std:: cout << '\n' << "Введите знак операции: +, -, * или /:";
+    char c; std:: cin >> c;
+
+    double answer;
+    bool error = false;
+    // if (c == '*') answer = first * second;                    <- условный оператор
+    // else if (c == '+') answer = first + second;
+    // else if (c == '-') answer = first - second;
+    // else if (c == '/') answer = first / second;
+    // else error = true;
+
+    switch (c) {
+        case '+': answer = first + second; break;             // <- switch-case
+        case '-': answer = first - second; break;
+        case '*': answer = first * second; break;
+        case '/': answer = first / second; break;
+        default: error = true;
+    }
+
+    if (error == false) std:: cout << first << " " << c << " " << second << " = " << answer;
+    else std:: cout << "Неправильный формат ввода\n";
+
+}
